d8600d8b16762ee9d276bb3c2d7a6e25f1ad2b64
NWUzmed <78712654+nwuzmedoutlook@users.noreply.github.com>
2022-10-27 17:22:36 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '0504ecf0cdb242b0ba3b886dbb55f9c7',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '我的个人主页',
+  domain: 'https://nwuzmed.notion.site/0504ecf0cdb242b0ba3b886dbb55f9c7',
+  author: '子穆二铎',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'NWUzmed',
+  github: 'nwuzmedoutlook',
+  linkedin: 'NWUzmed',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
